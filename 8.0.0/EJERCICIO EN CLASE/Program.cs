939cc05e7369ef05ff1a2c2df233cdb4cc3a0fe6
DONBETO72 <111794597+DONBETO72@users.noreply.github.com>
2022-11-14 18:20:40 -0600

    Add files via upload

---
+++ 8.0.0/EJERCICIO EN CLASE/Program.cs
@@ -0,0 +1,23 @@
+﻿double nota = 0;
+string respuesta;
+double suma = 0;
+double cant=0;
+double promedio = 0;
+
+// PEDIR NOTA
+do
+{
+    Console.WriteLine("Ingrese nota");
+    nota = Double.Parse(Console.ReadLine());
+    Console.WriteLine("Desea ingresar otra nota? Si/No");
+    respuesta = Console.ReadLine();
+
+    // NOTA ACUMULADA
+    suma = suma + nota;
+    cant++;
+}
+while (respuesta == "S");
+// salida
+promedio = suma / cant;
+
+Console.WriteLine("El promedio es de " + promedio );
